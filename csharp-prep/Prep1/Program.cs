3ad57d6d75c102a038550a5398b3430e2576dad2
Uchenna Godstime <princeuche08@gmail.com>
2023-01-02 05:11:51 +0400

    added file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
-        Console.WriteLine("My name is Uchenna");   
+        Console.WriteLine("My name is Uchenna"); 
+        Console.WriteLine("This is another line"); 
+        Console.Write("What is your favoirite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
     }
 }
